7a7b9dd9c14709637131daf96818d0652e5912bf
LeoAstora <147678182+LeoAstora@users.noreply.github.com>
2025-03-11 04:39:04 -0600

    Ejercicio 1

---
+++ Introduccion/app/src/main/java/com/marcoleo/introduccion/Variables.kt
@@ -0,0 +1,90 @@
+package com.marcoleo.introduccion
+
+import java.security.KeyStore.TrustedCertificateEntry
+import java.util.random.RandomGenerator.StreamableGenerator
+
+
+//VARIABLES
+fun main(){
+    val name = "Leonardo"
+
+    //VARIABLES NUMERICAS
+    val anos:Int = 10
+    val age:Long = 1000000000000000000
+    val flotante:Float = 30.4f
+    val dobles:Double = 123123.123
+
+    //VARIABLES ALFANUMERICAS
+
+    val caracter:Char = 'o'
+
+    //VARIABLE STRING
+
+    val cadena:String = "Hola Mundo uwu"
+
+    //VARIABLES BOOLEANAS
+
+    val booleanas:Boolean = true
+    val booleanas1:Boolean = false
+
+    var ano = 2
+    var ano2 = 12
+
+//    println("Suma")
+//    println(ano + ano2)
+//
+//    println("Resta")
+//    println(ano - ano2)
+//
+//    println("Multiplicacion")
+//    println(ano * ano2)
+//
+//    println("Division")
+//    println(ano2/ano)
+
+    var string1:String = "23"
+    var string2:String = "12"
+
+    var stringConcatenada:String = "Hola"
+    stringConcatenada = "Hola me llamo $name"
+    println(stringConcatenada)
+
+//    println(string1.toInt() + string2.toInt())
+}
+
+fun variablesNumericas(){
+
+    //VARIABLES NUMERICAS
+    val anos:Int = 10
+    val age:Long = 1000000000000000000
+    val flotante:Float = 30.4f
+    val dobles:Double = 123123.123
+
+    //VARIABLES ALFANUMERICAS
+
+    val caracter:Char = 'o'
+
+    //VARIABLE STRING
+
+    val cadena:String = "Hola Mundo uwu"
+
+    //VARIABLES BOOLEANAS
+
+    val booleanas:Boolean = true
+    val booleanas1:Boolean = false
+
+    var ano = 2
+    var ano2 = 12
+
+    println("Suma")
+    println(ano + ano2)
+
+    println("Resta")
+    println(ano - ano2)
+
+    println("Multiplicacion")
+    println(ano * ano2)
+
+    println("Division")
+    println(ano2/ano)
+}
